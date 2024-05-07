e5d28af5cd8e1257e0c7af939735b859e72955d9
ErikaNantia <82965675+ErikaNantia@users.noreply.github.com>
2024-05-07 22:51:04 +0200

    feat: add component book-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
 
 @Component({
   selector: 'bm-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'book-monkey';
 }
